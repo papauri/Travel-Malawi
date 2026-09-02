d2976deb9427da7bba80a81a2e9753caa35981da
papauri <papauri@users.noreply.github.com>
2026-09-02 19:08:35 +0100

    fix(build): increase pwa workbox cache file size limit to prevent build errors

--- vite.config.ts
+++ vite.config.ts
@@ -39,6 +39,7 @@ export default defineConfig(() => {
           ]
         },
         workbox: {
+          maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
           globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg}'],
           runtimeCaching: [
             {
